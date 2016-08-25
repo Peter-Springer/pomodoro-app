fe4ce53ca13948dcc66f4d9f4c8a502355f360b0
swanie21 <kswanie21@gmail.com>
2016-08-25 14:49:36 -0600

    create button integration tests

--- test/specs/test.js
+++ test/specs/test.js
@@ -8,21 +8,85 @@ describe('welcome page', function() {
    assert.equal(title, 'pomodoro-app');
   });
 });
-describe('buttons', function() {
+
+describe('start button', function() {
  it('should have a start button', function() {
    browser.url('/');
    assert.equal(browser.isExisting('#start-button'), true);
    });
- it('should have a pause button', function() {
+  it('should start the countdown', function() {
     browser.url('/');
-    assert.equal(browser.isExisting('#pause-button'), true);
-    });
-  it('should have a decrease button', function() {
+    var timer = browser.element('.timer');
+    browser.click('#start-button');
+    assert.equal(timer.getText(), 10);
+  });
+ });
+
+ describe('pause button', function(){
+   it('should have a pause button', function() {
      browser.url('/');
-     assert.equal(browser.isExisting('.decrease-time'), true);
-    });
-  it('should have a increase button', function() {
-      browser.url('/');
-      assert.equal(browser.isExisting('.increase-time'), true);
-    });
-});
+     assert.equal(browser.isExisting('#pause-button'), true);
+   });
+ });
+
+ describe('decrease session time button', function(){
+   it('should have a decrease session time button', function() {
+     browser.url('/');
+     assert.equal(browser.isExisting('.decrease-session-time'), true);
+   });
+   it('should decrease the time by one minute', function() {
+     browser.url('/');
+     browser.click('.decrease-session-time');
+     assert.equal(browser.getText('.session-duration'), 24);
+   });
+   it('should stop the time at 0', function() {
+     browser.url('/');
+     for (var i = 0; i < 25; i++) {
+       browser.click('.decrease-session-time');
+     }
+     assert.equal(browser.getText('.session-duration'), 0);
+   });
+ });
+
+ describe('increase session time button', function() {
+   it('should have an increase session time button', function() {
+     browser.url('/');
+     assert.equal(browser.isExisting('.increase-session-time'), true);
+   });
+   it('should increase the time by one minute', function() {
+     browser.url('/');
+     browser.click('.increase-session-time');
+     assert.equal(browser.getText('.session-duration'), 26);
+   });
+ });
+
+ describe('decrease break time button', function() {
+   it('should have a decrease break time button', function() {
+     browser.url('/');
+     assert.equal(browser.isExisting('.decrease-break-time'), true);
+   });
+   it('should decrease the time by one minute', function() {
+     browser.url('/');
+     browser.click('.decrease-break-time');
+     assert.equal(browser.getText('.break-duration'), 4);
+   });
+   it('should stop the time at 0', function() {
+     browser.url('/');
+     for (var i = 0; i < 5; i++) {
+       browser.click('.decrease-break-time');
+     }
+     assert.equal(browser.getText('.break-duration'), 0);
+   });
+ });
+
+ describe('increase break time button', function() {
+   it('should have an increase break time button', function() {
+     browser.url('/');
+     assert.equal(browser.isExisting('.increase-break-time'), true);
+   });
+   it('should increase the time by one minute', function() {
+     browser.url('/');
+     browser.click('.increase-break-time');
+     assert.equal(browser.getText('.break-duration'), 6);
+   });
+ });
