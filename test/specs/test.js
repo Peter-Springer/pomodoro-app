2f503133b887a6c52acd9657f41608a1bf5bb9b2
Peter Springer <peterspringer829@gmail.com>
2016-08-24 23:12:00 -0600

    start int testing

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,3 +1,4 @@
+'use strict';
 const assert = require('assert');
 
 describe('welcome page', function() {
@@ -6,4 +7,22 @@ describe('welcome page', function() {
    var title = browser.getTitle();
    assert.equal(title, 'pomodoro-app');
   });
+});
+describe('buttons', function() {
+ it('should have a start button', function() {
+   browser.url('/');
+   assert.equal(browser.isExisting('#start-button'), true);
+   });
+ it('should have a pause button', function() {
+    browser.url('/');
+    assert.equal(browser.isExisting('#pause-button'), true);
+    });
+  it('should have a decrease button', function() {
+     browser.url('/');
+     assert.equal(browser.isExisting('.decrease-time'), true);
+    });
+  it('should have a increase button', function() {
+      browser.url('/');
+      assert.equal(browser.isExisting('.increase-time'), true);
+    });
 });
