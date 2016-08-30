590ced37b31ce9e7726c7da5757a7536e7fc0327
swanie21 <kswanie21@gmail.com>
2016-08-30 13:17:31 -0600

    refactor and style disabled button

--- test/specs/test.js
+++ test/specs/test.js
@@ -2,6 +2,7 @@
 const assert = require('assert');
 
 describe('welcome page', function() {
+
  it('should be able to grab the page title', function() {
    browser.url('/');
    var title = browser.getTitle();
@@ -10,10 +11,12 @@ describe('welcome page', function() {
 });
 
 describe('start button', function() {
- it('should have a start button', function() {
+
+   it('should have a start button', function() {
    browser.url('/');
    assert.equal(browser.isExisting('#start-button'), true);
    });
+
   it('should start the countdown', function() {
     browser.url('/');
     var timer = browser.element('.timer');
@@ -24,6 +27,7 @@ describe('start button', function() {
  });
 
  describe('reset button', function() {
+
    it('should have a reset button', function() {
      browser.url('/');
      assert.equal(browser.isExisting('#reset-button'), true);
@@ -31,15 +35,18 @@ describe('start button', function() {
  });
 
  describe('decrease session time button', function() {
+
    it('should have a decrease session time button', function() {
      browser.url('/');
      assert.equal(browser.isExisting('.decrease-session-time'), true);
    });
+
    it('should decrease the time by one minute', function() {
      browser.url('/');
      browser.click('.decrease-session-time');
      assert.equal(browser.getText('.session-duration'), 24);
    });
+
    it('should stop the time at 0', function() {
      browser.url('/');
      for (var i = 0; i < 25; i++) {
@@ -47,6 +54,7 @@ describe('start button', function() {
      }
      assert.equal(browser.getText('.session-duration'), 0);
    });
+
    it('should decrease the session time text on the timer by 1', function() {
      browser.url('/');
      browser.click('.decrease-session-time');
@@ -57,15 +65,18 @@ describe('start button', function() {
  });
 
  describe('increase session time button', function() {
+
    it('should have an increase session time button', function() {
      browser.url('/');
      assert.equal(browser.isExisting('.increase-session-time'), true);
    });
+
    it('should increase the time by one minute', function() {
      browser.url('/');
      browser.click('.increase-session-time');
      assert.equal(browser.getText('.session-duration'), 26);
    });
+
    it('should stop incrementing the time at 59 minutes', function() {
      browser.url('/');
      for (var i = 0; i < 62; i++) {
@@ -73,6 +84,7 @@ describe('start button', function() {
      }
      assert.equal(browser.getText('.session-duration'), 59);
    });
+
    it('should increase the session time text on the timer by 1', function() {
      browser.url('/');
      browser.click('.increase-session-time');
@@ -83,15 +95,18 @@ describe('start button', function() {
  });
 
  describe('decrease break time button', function() {
+
    it('should have a decrease break time button', function() {
      browser.url('/');
      assert.equal(browser.isExisting('.decrease-break-time'), true);
    });
+
    it('should decrease the time by one minute', function() {
      browser.url('/');
      browser.click('.decrease-break-time');
      assert.equal(browser.getText('.break-duration'), 4);
    });
+
    it('should stop the time at 0', function() {
      browser.url('/');
      for (var i = 0; i < 5; i++) {
@@ -99,6 +114,7 @@ describe('start button', function() {
      }
      assert.equal(browser.getText('.break-duration'), 0);
    });
+
    it('should display the user break as they decrease break length', function() {
      browser.url('/');
      browser.click('.decrease-break-time');
@@ -108,15 +124,18 @@ describe('start button', function() {
  });
 
  describe('increase break time button', function() {
+   
    it('should have an increase break time button', function() {
      browser.url('/');
      assert.equal(browser.isExisting('.increase-break-time'), true);
    });
+
    it('should increase the time by one minute', function() {
      browser.url('/');
      browser.click('.increase-break-time');
      assert.equal(browser.getText('.break-duration'), 6);
    });
+
    it('should display the user break as they increase break length', function() {
      browser.url('/');
      browser.click('.increase-break-time');
